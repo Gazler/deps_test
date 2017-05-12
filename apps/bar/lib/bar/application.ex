cbd3c74d5086d29599f48d366da5716ec9e2c7e8
Gary Rennie <gazler@gmail.com>
2017-05-12 14:13:44 +0100

    Add an example for running in an async task

--- apps/bar/lib/bar/application.ex
+++ apps/bar/lib/bar/application.ex
@@ -10,6 +10,7 @@ defmodule Bar.Application do
 
     # Define workers and child supervisors to be supervised
     children = [
+      supervisor(Task.Supervisor, [[name: Bar.TaskSupervisor]])
       # Starts a worker by calling: Bar.Worker.start_link(arg1, arg2, arg3)
       # worker(Bar.Worker, [arg1, arg2, arg3]),
     ]
--- deps.exs
+++ deps.exs
@@ -1,14 +1,41 @@
+{:ok, cwd} = File.cwd()
 deps = Mix.Dep.Umbrella.loaded()
-File.cd("/tmp")
-new_deps = Mix.Dep.Umbrella.loaded()
-if deps == new_deps do
-  IO.puts """
-  #{IO.ANSI.green()}Deps match#{IO.ANSI.reset()}
-  """
-else
-  IO.puts """
-  #{IO.ANSI.red()}Deps diverge#{IO.ANSI.reset()}
-  before #{deps |> Enum.map(&(&1.from)) |> inspect}
-  after: #{new_deps |> Enum.map(&(&1.from)) |> inspect}
-  """
+
+check_deps = fn ->
+  new_deps = Mix.Dep.Umbrella.loaded()
+  if deps == new_deps do
+    IO.puts """
+    #{IO.ANSI.green()}Deps match#{IO.ANSI.reset()}
+    """
+  else
+    IO.puts """
+    #{IO.ANSI.red()}Deps diverge#{IO.ANSI.reset()}
+    before #{deps |> Enum.map(&(&1.from)) |> inspect}
+    after: #{new_deps |> Enum.map(&(&1.from)) |> inspect}
+    """
+  end
 end
+
+IO.puts "Running with local File.cd"
+File.cd("/tmp")
+check_deps.()
+
+IO.puts "Running afer cd back to initial directory"
+File.cd(cwd)
+check_deps.()
+
+parent = self()
+
+IO.puts "Running with async task"
+
+Task.Supervisor.async_nolink(Bar.TaskSupervisor, fn ->
+  File.mkdir_p("/tmp/broken_deps")
+  File.cd("/tmp/broken_deps")
+  send(parent, :done)
+end)
+
+receive do
+  :done -> :ok
+end
+
+check_deps.()
